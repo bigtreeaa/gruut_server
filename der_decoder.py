3b96c78b010aba3b8d31703e7b35b625610fa9d7
Junghye Choi <65644118+bigtreeaa@users.noreply.github.com>
2023-07-03 16:39:27 +0900

    generate DER decoder files
    use to decode asn1 DER format

---
+++ der_decoder.py
@@ -0,0 +1,8 @@
+from bitarray import bitarray
+from bitarray.util import serialize, deserialize
+import base64
+
+base64_signature = "MEUCIAH3d2zS0XwSXd0HzF7q8m4JE5+ivuQdlgVLJLU7FwGvAiEA6ewMkPF5QCKMA2BlmpIzl3ttS8X7CJ2rhh6OirdIVC8="
+
+
+if __name__ == "__main__":
