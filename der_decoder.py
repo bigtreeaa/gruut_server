a12173e0bf0f9301c713bcb64e7394dba6cfdac2
Junghye Choi <65644118+bigtreeaa@users.noreply.github.com>
2023-07-03 20:54:10 +0900

    Code DER decoder

--- der_decoder.py
+++ der_decoder.py
@@ -1,8 +1,23 @@
-from bitarray import bitarray
-from bitarray.util import serialize, deserialize
-import base64
+from bitarray.util import hex2ba
 
-base64_signature = "MEUCIAH3d2zS0XwSXd0HzF7q8m4JE5+ivuQdlgVLJLU7FwGvAiEA6ewMkPF5QCKMA2BlmpIzl3ttS8X7CJ2rhh6OirdIVC8="
+hex_signature = '30450220203D56DC1D7C1EF988570F7B9D74DC96EA677D7FFDB94C09BED9C2CF5D967134022100AB8A49160B526F83D9FF4BDC58C1B4DA836E2A6741289562284D334292A970BA'
+
+
+def der_decoder(signature):
+    signature = signature[12:]  # remove heading
+    hex_r = signature[:64]  # get number r
+    ba_r = hex2ba(hex_r, endian='little')
+    if ba_r[0] == 1:
+        signature = signature[66:]
+    else:
+        signature = signature[64:]
+    hex_s = signature[:64]
+    signature = hex_r + hex_s
+    return signature
 
 
 if __name__ == "__main__":
+    signature = der_decoder(hex_signature)
+    print(signature)
+    print(len(signature))
+
